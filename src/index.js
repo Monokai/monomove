8bee927574f37f57e99c48980baf874ff71d0457
Monokai <wimer@monokai.nl>
2021-08-15 09:16:38 +0200

    also for require()

--- src/index.js
+++ src/index.js
@@ -7,5 +7,5 @@ import MMTween from './move/Tween';
 
 export const delay = async x => await new Tween(() => true, x).start();
 export const Tween = MMTween;
-export const SmoothScroller = require('move/SmoothScroller').default;
-export const RenderLoop = require('move/RenderLoop').default;
+export const SmoothScroller = require('./move/SmoothScroller').default;
+export const RenderLoop = require('./move/RenderLoop').default;
