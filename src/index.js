3d8253a85e6d4e05ad274862d039a71d7dfdbe2e
Monokai <wimer@monokai.nl>
2021-08-17 08:08:52 +0200

    export tween manager

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@
 */
 
 import MMTween from './move/Tween';
+import MMTweenManager from './move/TweenManager';
 import MMSmoothScroller from './move/SmoothScroller';
 import MMRenderLoop from './move/RenderLoop';
 
 export const delay = async x => await new MMTween(() => true, x).start();
 export const Tween = MMTween;
+export const TweenManager = MMTweenManager;
 export const SmoothScroller = MMSmoothScroller;
 export const RenderLoop = MMRenderLoop;
