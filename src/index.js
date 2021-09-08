c42c7e686ba2a03e667103370f7cc811a040f244
Monokai <wimer@monokai.nl>
2021-09-08 08:50:24 +0200

    delay fix

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@
 	Utilities for moving things on screen
 */
 
-export {default as Tween} from './move/Tween';
+import MMTween from './move/Tween';
+
 export {default as TweenManager} from './move/TweenManager';
 export {default as Timeline} from './move/Timeline';
 export {default as SmoothScroller} from './move/SmoothScroller';
 export {default as RenderLoop} from './move/RenderLoop';
+
+export const Tween = MMTween;
 
 export const delay = async x => await new Tween(() => true, x).start();
