2c03111429053a95127ce3deb645ecbf67bb0d75
Monokai <wimer@monokai.nl>
2022-06-07 11:04:10 +0200

    trigger final animation states on devices that can't keep up with intersection observers

--- src/move/SmoothScroller.js
+++ src/move/SmoothScroller.js
@@ -21,6 +21,54 @@ try {
 
 export default class SmoothScroller {
 
+	isDown = false;
+	isLocked = false;
+	scroll = 0;
+	scrollHeight = 0;
+
+	#scrollThreshold = 0.01;
+	#touchScrollFactor = 1;
+	#targetScroll = 0;
+	#previousScroll = 0;
+	#height = 0;
+	#animations = [];
+	#activeAnimations = null;
+	#debugCanvas = null;
+	#debugContext = null;
+	#isAnimating = false;
+	#previousTime = 0;
+	#previousScrollHeight = 0;
+	#touchStartListener = null;
+	#scrollListener = null;
+	#wheelListener = null;
+	#mouseDownListener = null;
+	#isFirstScrollInstant = true;
+	#isTouch = false;
+	#isScrollingToAnchor = false;
+	#scrollTween = new Tween()
+		.easing('0.35,0.15,0,1')
+		.onUpdate(o => {
+			window.scrollTo(0, o.y);
+			this.#isAnimating = true;
+			this.scroll = o.y;
+			this.#targetScroll = o.y;
+			this.isDown = this.#targetScroll >= this.#previousScroll;
+			this.#previousScroll = this.#targetScroll;
+		})
+		.onStart(() => {
+			this.#isScrollingToAnchor = true;
+		})
+		.onComplete(() => {
+			this.#isScrollingToAnchor = false;
+		});
+
+	#passive = supportsPassiveListeners ? {passive: true} : false;
+	#scrollFactor = null;
+	#container = null;
+	#content = null;
+	#listener = null;
+	#debug = null;
+
 	constructor({
 		container = window.document.body,
 		content = window.document.body,
@@ -28,135 +76,111 @@ export default class SmoothScroller {
 		listener = window,
 		debug = false
 	} = {}) {
-		this.isLocked = false;
-		this.scrollThreshold = 0.01;
-		this.touchScrollFactor = 1;
-		this.targetScroll = 0;
-		this.scroll = 0;
-		this.previousScroll = 0;
-		this.height = 0;
-		this.isDown = false;
-		this.animations = [];
-		this.activeAnimations = null;
-		this.debugCanvas = null;
-		this.debugContext = null;
-		this.isAnimating = false;
-		this.previousTime = 0;
-		this.scrollHeight = 0;
-		this.previousScrollHeight = 0;
-		this.touchStartListener = null;
-		this.scrollListener = null;
-		this.wheelListener = null;
-		this.isFirstScrollInstant = true;
-		this.isTouch = false;
-		this.isScrollingToAnchor = false;
-		this.scrollTween = new Tween()
-			.easing('0.35,0.15,0,1')
-			.onUpdate(o => {
-				window.scrollTo(0, o.y);
-				this.isAnimating = true;
-				this.scroll = o.y;
-				this.targetScroll = o.y;
-				this.isDown = this.targetScroll >= this.previousScroll;
-				this.previousScroll = this.targetScroll;
-			})
-			.onStart(() => {
-				this.isScrollingToAnchor = true;
-			})
-			.onComplete(() => {
-				this.isScrollingToAnchor = false;
-			});
+		this.#scrollFactor = scrollFactor;
+		this.#container = container;
+		this.#content = content;
+		this.#listener = listener;
+		this.#debug = debug;
 
-		this.passive = supportsPassiveListeners ? {passive: true} : false;
-		this.scrollFactor = scrollFactor;
-		this.container = container;
-		this.content = content;
-		this.listener = listener;
-		this.debug = debug;
-
-		this.touchStartListener = () => {
+		this.#touchStartListener = () => {
 			if (this.isLocked) {
 				return;
 			}
 
-			this.isTouch = true;
-			// todo: doesn't reset to initial state when using mouse after touch
+			this.#isTouch = true;
 		};
 
-		this.scrollListener = () => {
+		this.#scrollListener = () => {
 			if (this.isLocked) {
 				return;
 			}
 
-			this.isAnimating = true;
-			this.targetScroll = this.getScrollPosition();
+			this.#isAnimating = true;
+			this.#targetScroll = this.getScrollPosition();
 		};
 
-		this.wheelListener = () => {
+		this.#mouseDownListener = e => {
+			e.stopPropagation();
+
 			if (this.isLocked) {
 				return;
 			}
 
-			if (this.scrollTween) {
-				this.scrollTween.stop();
-			}
-
-			this.isScrollingToAnchor = false;
+			this.#isTouch = false;
 		};
 
-		if (this.listener) {
-			if (this.debug) {
+		this.#wheelListener = () => {
+			if (this.isLocked) {
+				return;
+			}
+
+			if (this.#scrollTween) {
+				this.#scrollTween.stop();
+			}
+
+			this.#isTouch = false;
+			this.#isScrollingToAnchor = false;
+		};
+
+		if (this.#listener) {
+			if (this.#debug) {
 				console.log('add scroll listeners');
 			}
 
-			this.listener.addEventListener('touchstart', this.touchStartListener, this.passive);
-			this.listener.addEventListener('scroll', this.scrollListener, this.passive);
-			this.listener.addEventListener('wheel', this.wheelListener, this.passive);
+			this.#listener.addEventListener('touchstart', this.#touchStartListener, this.#passive);
+			this.#listener.addEventListener('scroll', this.#scrollListener, this.#passive);
+			this.#listener.addEventListener('mousedown', this.#mouseDownListener, this.#passive);
+			this.#listener.addEventListener('wheel', this.#wheelListener, this.#passive);
 		}
 
-		if (this.debug) {
-			this.debugCanvas = window.document.createElement('canvas');
-			this.container.appendChild(this.debugCanvas);
+		if (this.#debug) {
+			this.#debugCanvas = window.document.createElement('canvas');
+			this.#container.appendChild(this.#debugCanvas);
 		}
 
-		RenderLoop.add(this, this.onTick);
+		RenderLoop.add(this, this.#onTick);
 
 		this.resize();
 	}
 
-	onTick() {
+	#onTick() {
 		if (this.isLocked) {
 			return true;
 		}
 
-		this.scrollHeight = this.content ? this.content.scrollHeight : 0;
+		this.scrollHeight = this.#content ? this.#content.scrollHeight : 0;
 
-		if (this.scrollHeight !== this.previousScrollHeight) {
+		if (this.scrollHeight !== this.#previousScrollHeight) {
 			this.resize();
-			this.scroll = this.targetScroll;
-			this.onScroll(this.scroll);
-			this.previousScrollHeight = this.scrollHeight;
+			this.scroll = this.#targetScroll;
+			this.#onScroll(this.scroll);
+			this.#previousScrollHeight = this.scrollHeight;
 
 			return true;
 		}
 
-		if (!this.isAnimating) {
+		if (!this.#isAnimating) {
 			return true;
 		}
 
-		const dy = (this.targetScroll - this.scroll) * (this.isTouch ? this.touchScrollFactor : this.scrollFactor);
+		const dy = (this.#targetScroll - this.scroll) * (this.#isTouch ? this.#touchScrollFactor : this.#scrollFactor);
 
-		if (Math.abs(dy) < this.scrollThreshold) {
-			this.isAnimating = false;
+		if (Math.abs(dy) < this.#scrollThreshold) {
+			console.log('stop scrolling');
+
+			// trigger final animations on slow devices that can't keep up with intersection observers
+			this.#onScroll(this.scroll);
+
+			this.#isAnimating = false;
 		}
 
 		this.scroll += dy;
 
-		if (this.isFirstScrollInstant) {
-			this.isFirstScrollInstant = false;
-			this.scroll = this.targetScroll;
+		if (this.#isFirstScrollInstant) {
+			this.#isFirstScrollInstant = false;
+			this.scroll = this.#targetScroll;
 
-			if (this.debug) {
+			if (this.#debug) {
 				console.log('trigger all animations in both directions');
 			}
 
@@ -169,85 +193,85 @@ export default class SmoothScroller {
 			this.isDown = tempIsDown;
 		}
 
-		if (this.activeAnimations === null) {
-			this.activeAnimations = this.animations;
+		if (this.#activeAnimations === null) {
+			this.#activeAnimations = this.#animations;
 		}
 
-		this.onScroll(this.scroll);
+		this.#onScroll(this.scroll);
 
 		return true;
 	}
 
 	draw() {
-		this.onScroll();
+		this.#onScroll();
 	}
 
 	drawAll() {
-		const animations = this.activeAnimations;
+		const animations = this.#activeAnimations;
 
-		this.activeAnimations = this.animations;
-		this.onScroll();
-		this.activeAnimations = animations;
+		this.#activeAnimations = this.#animations;
+		this.#onScroll();
+		this.#activeAnimations = animations;
 	}
 
 	getScrollPosition() {
-		if (this.listener.scrollY !== undefined) {
-			return this.listener.scrollY;
+		if (this.#listener.scrollY !== undefined) {
+			return this.#listener.scrollY;
 		}
 
-		if (this.listener.pageYOffset !== undefined) {
-			return this.listener.pageYOffset;
+		if (this.#listener.pageYOffset !== undefined) {
+			return this.#listener.pageYOffset;
 		}
 
-		if (this.listener.scrollTop !== undefined) {
-			return this.listener.scrollTop;
+		if (this.#listener.scrollTop !== undefined) {
+			return this.#listener.scrollTop;
 		}
 
 		return 0;
 	}
 
 	resize() {
-		if (!this.container || !this.content) {
+		if (!this.#container || !this.#content) {
 			return;
 		}
 
-		this.scroll = this.previousScroll = this.targetScroll;
-		this.scrollHeight = this.content.scrollHeight;
-		this.height = window.innerHeight;
+		this.scroll = this.#previousScroll = this.#targetScroll;
+		this.scrollHeight = this.#content.scrollHeight;
+		this.#height = window.innerHeight;
 
-		this.animations.forEach(a => {
-			this.initBox(a);
+		this.#animations.forEach(a => {
+			this.#initBox(a);
 		});
 
-		if (this.debugCanvas) {
-			this.debugCanvas.width = window.innerWidth * window.devicePixelRatio;
-			this.debugCanvas.height = window.innerHeight * window.devicePixelRatio;
+		if (this.#debugCanvas) {
+			this.#debugCanvas.width = window.innerWidth * window.devicePixelRatio;
+			this.#debugCanvas.height = window.innerHeight * window.devicePixelRatio;
 
-			this.debugCanvas.style.position = 'fixed';
-			this.debugCanvas.style.left = 0;
-			this.debugCanvas.style.top = 0;
-			this.debugCanvas.style.width = `${window.innerWidth}px`;
-			this.debugCanvas.style.height = `${window.innerHeight}px`;
-			this.debugCanvas.style.pointerEvents = 'none';
-			this.debugCanvas.style.zIndex = 9999999;
+			this.#debugCanvas.style.position = 'fixed';
+			this.#debugCanvas.style.left = 0;
+			this.#debugCanvas.style.top = 0;
+			this.#debugCanvas.style.width = `${window.innerWidth}px`;
+			this.#debugCanvas.style.height = `${window.innerHeight}px`;
+			this.#debugCanvas.style.pointerEvents = 'none';
+			this.#debugCanvas.style.zIndex = 9999999;
 
-			this.debugContext = this.debugCanvas.getContext('2d');
+			this.#debugContext = this.#debugCanvas.getContext('2d');
 		}
 
-		this.updateActiveAnimations();
+		this.#updateActiveAnimations();
 	}
 
 	triggerAnimations(all = false) {
 		const r = window.devicePixelRatio;
 
-		if (this.debugContext) {
-			this.debugContext.clearRect(0, 0, this.debugCanvas.width, this.debugCanvas.height);
-			this.debugContext.strokeStyle = '#f00';
-			this.debugContext.lineWidth = 2 * r;
-			this.debugContext.beginPath();
+		if (this.#debugContext) {
+			this.#debugContext.clearRect(0, 0, this.#debugCanvas.width, this.#debugCanvas.height);
+			this.#debugContext.strokeStyle = '#f00';
+			this.#debugContext.lineWidth = 2 * r;
+			this.#debugContext.beginPath();
 		}
 
-		const animations = all ? this.animations : this.activeAnimations;
+		const animations = all ? this.#animations : this.#activeAnimations;
 
 		animations.forEach(a => {
 			const box = a.box;
@@ -262,9 +286,14 @@ export default class SmoothScroller {
 
 			const directionOffset = a.directionOffset || 0;
 			const offset = a.offset || 0;
-			const pos = box.top + box.height - o.scroll + directionOffset * (this.isDown ? -1 : 1) * this.height + offset * this.height;
-			const rawFactor = pos / (this.height + box.height);
-			const rawBoxFactor = (pos - this.height) / box.height;
+			const pos = box.top
+				+ box.height
+				- o.scroll
+				+ directionOffset * (this.isDown ? -1 : 1) * this.#height
+				+ offset * this.#height;
+
+			const rawFactor = pos / (this.#height + box.height);
+			const rawBoxFactor = (pos - this.#height) / box.height;
 
 			o.rawFactor = ((1 - rawFactor) - 0.5) * a.speed + 0.5;
 			o.rawBoxFactor = ((1 - rawBoxFactor) - 0.5) * a.speed + 0.5;
@@ -313,26 +342,34 @@ export default class SmoothScroller {
 
 			o.previousIsInView = o.isInView;
 
-			if (this.debugContext && (o.box.top + o.box.height - o.scroll >= 0 && o.box.top - o.scroll <= this.height)) {
-				this.debugContext.rect(o.box.left * r, o.box.top * r - o.scroll * r, o.box.width * r, o.box.height * r);
+			if (this.#debugContext
+				&& (o.box.top + o.box.height - o.scroll >= 0
+				&& o.box.top - o.scroll <= this.#height)
+			) {
+				this.#debugContext.rect(
+					o.box.left * r,
+					o.box.top * r - o.scroll * r,
+					o.box.width * r,
+					o.box.height * r
+				);
 			}
 		});
 
-		if (this.debugContext) {
-			this.debugContext.stroke();
+		if (this.#debugContext) {
+			this.#debugContext.stroke();
 		}
 	}
 
-	onScroll(scroll = 0) {
-		this.isDown = scroll >= this.previousScroll;
+	#onScroll(scroll = 0) {
+		this.isDown = scroll >= this.#previousScroll;
 
-		if (!this.activeAnimations) {
+		if (!this.#activeAnimations) {
 			return;
 		}
 
 		this.triggerAnimations();
 
-		this.previousScroll = scroll;
+		this.#previousScroll = scroll;
 	}
 
 	add(_items, callback, options = {}) {
@@ -357,7 +394,7 @@ export default class SmoothScroller {
 				observer = new window.IntersectionObserver(entries => {
 					entries.forEach(entry => {
 						animationObject.isVisible = entry.isIntersecting;
-						this.updateActiveAnimations();
+						this.#updateActiveAnimations();
 					});
 				}, {
 					root      : options.observeIn,
@@ -367,7 +404,7 @@ export default class SmoothScroller {
 
 				observer.observe(item);
 			} else {
-				this.activeAnimations = null;
+				this.#activeAnimations = null;
 			}
 
 			const animation = {
@@ -380,24 +417,24 @@ export default class SmoothScroller {
 				observer
 			};
 
-			this.initBox(animation);
+			this.#initBox(animation);
 
-			this.animations.push(animation);
+			this.#animations.push(animation);
 		});
 	}
 
-	updateActiveAnimations() {
-		this.activeAnimations = this.animations.filter(a => a.animationObject.isVisible);
+	#updateActiveAnimations() {
+		this.#activeAnimations = this.#animations.filter(a => a.animationObject.isVisible);
 
-		if (this.debug) {
-			console.log(`active animations: ${this.activeAnimations.length}`);
+		if (this.#debug) {
+			console.log(`active animations: ${this.#activeAnimations.length}`);
 		}
 	}
 
 	remove(_items) {
 		const items = _items && _items instanceof Array ? _items : [_items];
 
-		this.animations = this.animations.filter(a => !items.includes(a.item));
+		this.#animations = this.#animations.filter(a => !items.includes(a.item));
 	}
 
 	static getBox(node) {
@@ -440,21 +477,21 @@ export default class SmoothScroller {
 		};
 	}
 
-	initBox(a) {
+	#initBox(a) {
 		a.box = SmoothScroller.getBox(a.item);
-		a.animationObject.centerOffset = (this.height - a.box.height) * 0.5;
+		a.animationObject.centerOffset = (this.#height - a.box.height) * 0.5;
 		a.animationObject.originalTop = a.box.top;
-		a.animationObject.scroll = a.animationObject.targetScroll = this.targetScroll;
+		a.animationObject.scroll = a.animationObject.targetScroll = this.#targetScroll;
 	}
 
 	scrollTo(position = 0, time = null) {
-		return this.scrollTween
+		return this.#scrollTween
 			.from({
 				y: this.scroll
 			})
 			.to({
 				y: position
-			}, time ? time : clamp(Math.abs(position - this.scroll) * 0.00025, 0.25, 2.5))
+			}, time ?? clamp(Math.abs(position - this.scroll) * 0.00025, 0.25, 2.5))
 			.start();
 	}
 
@@ -465,12 +502,12 @@ export default class SmoothScroller {
 	}
 
 	reset() {
-		this.animations.length = 0;
-		this.height = 0;
+		this.#animations.length = 0;
+		this.#height = 0;
 	}
 
 	stop() {
-		this.isAnimating = false;
+		this.#isAnimating = false;
 	}
 
 	lock() {
@@ -483,27 +520,28 @@ export default class SmoothScroller {
 	}
 
 	setContent(content) {
-		this.content = content;
+		this.#content = content;
 		this.resize();
 	}
 
 	unsetContent() {
-		this.content = null;
+		this.#content = null;
 	}
 
 	destroy() {
-		if (this.debug) {
-			this.debugCanvas.remove();
+		if (this.#debug) {
+			this.#debugCanvas.remove();
 		}
 
-		if (this.listener) {
-			if (this.debug) {
+		if (this.#listener) {
+			if (this.#debug) {
 				console.log('remove scroll listeners');
 			}
 
-			this.listener.removeEventListener('touchstart', this.touchStartListener, this.passive);
-			this.listener.removeEventListener('wheel', this.WheelListener, this.passive);
-			this.listener.removeEventListener('scroll', this.scrollListener, this.passive);
+			this.#listener.removeEventListener('touchstart', this.#touchStartListener, this.#passive);
+			this.#listener.removeEventListener('wheel', this.#wheelListener, this.#passive);
+			this.#listener.removeEventListener('mousedown', this.#mouseDownListener, this.#passive);
+			this.#listener.removeEventListener('scroll', this.#scrollListener, this.#passive);
 		}
 
 		this.reset();
